3bac5a9136113b1c9d4696e9d8045b9bdf2430ae
Edwin <edwin.charlotte@etu.univ-orleans.fr>
2022-04-05 22:26:26 +0200

    ajout header dans social fonctionnel

--- micro-social/src/main/java/steam/microsocial/Controlleur/ControllerSocial.java
+++ micro-social/src/main/java/steam/microsocial/Controlleur/ControllerSocial.java
@@ -13,7 +13,11 @@ import steam.microsocial.Entities.Social;
 import steam.microsocial.Repository.RepositorySocial;
 import steam.microsocial.Service.ServiceSocial;
 
+import java.io.IOException;
 import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Collection;
 
 @RestController
@@ -22,83 +26,128 @@ public class ControllerSocial {
     @Autowired
     private ServiceSocial serviceSocial;
 
+    private final static String URI_CLIENT= "http://localhost:8080/client/token?token=";
+
     @PostMapping(value = "/message")
-    public ResponseEntity<String> addMessage(@RequestBody Message message){
-     /* HttpClient httpClient = HttpClient.newHttpClient();
-        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(URI_Catalogue+"?token="+token)).GET().build();*/
-        try{
-            serviceSocial.sendNewMessage(message);
-            return ResponseEntity.created(URI.create("/social/message/" + message.getIdMessage())).body("Votre message a bien été envoyé à " + message.getReceveur());
-        } catch (Exception e) {
-            e.printStackTrace();
+    public ResponseEntity<String> addMessage(@RequestBody Message message, @RequestHeader(name="token") String token){
+        if (checkToken(token))
+        {
+            try{
+                serviceSocial.sendNewMessage(message);
+                return ResponseEntity.created(URI.create("/social/message/" + message.getIdMessage())).body("Votre message a bien été envoyé à " + message.getReceveur());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
         }
-        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
+        else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 
     @GetMapping(value = "/message/{id}")
-    public ResponseEntity<Collection<Social>> getMessageJoueurEnvoye(@PathVariable int id){
-/*      HttpClient httpClient = HttpClient.newHttpClient();
-        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(URI_Catalogue+"?token="+token)).GET().build();*/
-        try{
-            Collection<Social> social = serviceSocial.getSocialAll();
-            ResponseEntity<Collection<Social>> responseEntity = ResponseEntity.ok(social);
-            return responseEntity;
-        } catch (Exception e) {
-            e.printStackTrace();
+    public ResponseEntity<Collection<Social>> getMessageJoueurEnvoye(@PathVariable int id, @RequestHeader(name="token") String token){
+        if (checkToken(token)){
+            try{
+                Collection<Social> social = serviceSocial.getSocialAll();
+                ResponseEntity<Collection<Social>> responseEntity = ResponseEntity.ok(social);
+                return responseEntity;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
         }
-        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
+        else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 
     @GetMapping(value = "/message")
-    public ResponseEntity<Collection<Social>> getMessageJoueurAll(){
-/*      HttpClient httpClient = HttpClient.newHttpClient();
-        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(URI_Catalogue+"?token="+token)).GET().build();*/
-        try{
-            Collection<Social> social = serviceSocial.getSocialAll();
-            ResponseEntity<Collection<Social>> responseEntity = ResponseEntity.ok(social);
-            return responseEntity;
-        } catch (Exception e) {
-            e.printStackTrace();
+    public ResponseEntity<Collection<Social>> getMessageJoueurAll(@RequestHeader(name="token") String token){
+        if (checkToken(token)){
+            try{
+                Collection<Social> social = serviceSocial.getSocialAll();
+                ResponseEntity<Collection<Social>> responseEntity = ResponseEntity.ok(social);
+                return responseEntity;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
         }
-        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
+        else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 
     @GetMapping(value = "/recevoir")
-    public ResponseEntity<Collection<Receveur>> getMessageJoueurRecuAll(){
-/*      HttpClient httpClient = HttpClient.newHttpClient();
-        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(URI_Catalogue+"?token="+token)).GET().build();*/
-        try{
-            Collection<Receveur> receveur = serviceSocial.getRecveurAll();
-            ResponseEntity<Collection<Receveur>> responseEntity = ResponseEntity.ok(receveur);
-            return responseEntity;
-        } catch (Exception e) {
-            e.printStackTrace();
+    public ResponseEntity<Collection<Receveur>> getMessageJoueurRecuAll(@RequestHeader(name="token") String token){
+        if (checkToken(token)){
+            try{
+                Collection<Receveur> receveur = serviceSocial.getRecveurAll();
+                ResponseEntity<Collection<Receveur>> responseEntity = ResponseEntity.ok(receveur);
+                return responseEntity;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
         }
-        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
+        else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 
     @DeleteMapping(value = "/message/{idMessage}/{idEnvoyeur}")
-    public ResponseEntity<String> deleteMessageById(@PathVariable Integer idEnvoyeur, @PathVariable Integer idMessage){
-        try{
-            System.out.println("id envoyeur : " + idEnvoyeur);
-            System.out.println("id envoyeur : " + idMessage);
-            serviceSocial.deleteMessage(idEnvoyeur, idMessage);
-            return ResponseEntity.created(URI.create("/social/message/" + idMessage)).body("Votre message << " + idMessage + " >> a bien été supprimé");
-        } catch (UnknownIdMessageException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("le message passé est incorrect ou n'existe pas");
-        } catch (UnknownEnvoyeurException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("vous n'etes pas connecté");
+    public ResponseEntity<String> deleteMessageById(@PathVariable Integer idEnvoyeur, @PathVariable Integer idMessage, @RequestHeader(name="token") String token){
+        if (checkToken(token)){
+            try{
+                System.out.println("id envoyeur : " + idEnvoyeur);
+                System.out.println("id envoyeur : " + idMessage);
+                serviceSocial.deleteMessage(idEnvoyeur, idMessage);
+                return ResponseEntity.created(URI.create("/social/message/" + idMessage)).body("Votre message << " + idMessage + " >> a bien été supprimé");
+            } catch (UnknownIdMessageException e) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("le message passé est incorrect ou n'existe pas");
+            } catch (UnknownEnvoyeurException e) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("vous n'etes pas connecté");
+            }
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
     }
 
     @DeleteMapping(value = "/{idSocial}")
-    public ResponseEntity<String> deleteSocialById(@PathVariable Integer idSocial){
-        try {
-            serviceSocial.deleteSocial(idSocial);
-            return ResponseEntity.created(URI.create("/social/message/" + idSocial)).body("Votre message << " + idSocial + " >> a bien été supprimé");
-        } catch (Exception e) {
-            e.printStackTrace();
+    public ResponseEntity<String> deleteSocialById(@PathVariable Integer idSocial, @RequestHeader(name="token") String token){
+        if (checkToken(token)){
+            try {
+                serviceSocial.deleteSocial(idSocial);
+                return ResponseEntity.created(URI.create("/social/message/" + idSocial)).body("Votre message << " + idSocial + " >> a bien été supprimé");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
         }
-        return null;
+        else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+    public boolean checkToken(String token){
+        HttpClient httpClient = HttpClient.newHttpClient();
+        try {
+            HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(URI_CLIENT+token)).GET().build();
+            HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 200){
+                return true;
+            }
+            else {
+                return false;
+            }
+        } catch (IOException e) {
+            return false;
+        } catch (InterruptedException e) {
+            return false;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
