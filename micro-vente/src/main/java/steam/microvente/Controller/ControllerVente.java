653bf76417190275382548f0514a72f796d06666
Edwin <edwin.charlotte@etu.univ-orleans.fr>
2022-04-05 22:03:29 +0200

    ajout header dans vente fonctionnel

--- micro-vente/src/main/java/steam/microvente/Controller/ControllerVente.java
+++ micro-vente/src/main/java/steam/microvente/Controller/ControllerVente.java
@@ -13,6 +13,7 @@ import steam.microvente.Service.ServiceVente;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -48,66 +49,91 @@ public class ControllerVente {
     }
 
     @GetMapping(value = "/jeu/{id}")
-    public ResponseEntity<Collection<Vente>> listeVentesPourJeu(@PathVariable String id){
-        try{
-            Collection<Vente> listeAllVente = serviceVente.getVentesByGameId(Integer.valueOf(id));
-            if (listeAllVente.size() == 0){
-                return ResponseEntity.noContent().build();
+    public ResponseEntity<Collection<Vente>> listeVentesPourJeu(@PathVariable String id, @RequestHeader(name="token") String token){
+        if (checkToken(token)){
+            try{
+                Collection<Vente> listeAllVente = serviceVente.getVentesByGameId(Integer.valueOf(id));
+                if (listeAllVente.size() == 0){
+                    return ResponseEntity.noContent().build();
+                }
+                else{
+                    ResponseEntity<Collection<Vente>> responseEntity = ResponseEntity.ok(listeAllVente);
+                    return responseEntity;
+                }
+            } catch (IdGameUnkownException e) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
             }
-            else{
-                ResponseEntity<Collection<Vente>> responseEntity = ResponseEntity.ok(listeAllVente);
-                return responseEntity;
-            }
-        } catch (IdGameUnkownException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
     }
 
     @GetMapping(value = "/client/{id}")
-    public ResponseEntity<Collection<Bibliotheque>> listeVentesPourClient(@PathVariable String id) throws IdClientUnknownException {
-        Collection<Bibliotheque> listeAllVente = serviceVente.getVentesByClientId(Integer.valueOf(id));
-        if (listeAllVente.size() == 0){
-            return ResponseEntity.noContent().build();
+    public ResponseEntity<Collection<Bibliotheque>> listeVentesPourClient(@PathVariable String id, @RequestHeader(name="token") String token) throws IdClientUnknownException {
+        if (checkToken(token)){
+            try{
+                Collection<Bibliotheque> listeAllVente = serviceVente.getVentesByClientId(Integer.valueOf(id));
+                if (listeAllVente.size() == 0){
+                    return ResponseEntity.noContent().build();
+                }
+                else{
+                    ResponseEntity<Collection<Bibliotheque>> responseEntity = ResponseEntity.ok(listeAllVente);
+                    return responseEntity;
+                }
+            }
+            catch (IdClientUnknownException e){
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
         }
         else{
-            ResponseEntity<Collection<Bibliotheque>> responseEntity = ResponseEntity.ok(listeAllVente);
-            return responseEntity;
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
     }
 
     @PostMapping(value = "/")
-    public ResponseEntity<String> addJeu(@RequestBody Achat achat){
-        try{
-            serviceVente.buyGame(achat);
-            return ResponseEntity.created(URI.create("/jeu/" + achat.getIdJeu())).body(achat.getIdClient() + " a achété le jeu " + achat.getIdJeu());
-        } catch (IdGameUnkownException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Id du jeu inconnu");
-        } catch (IdClientUnknownException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Id de client inconnu");
-        } catch (GameAlreadyOwnedException e) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(achat.getIdClient() + " a déjà achété ce jeu");
+    public ResponseEntity<String> addJeu(@RequestBody Achat achat, @RequestHeader(name="token") String token){
+        if (checkToken(token)){
+            try{
+                serviceVente.buyGame(achat);
+                return ResponseEntity.created(URI.create("/jeu/" + achat.getIdJeu())).body(achat.getIdClient() + " a achété le jeu " + achat.getIdJeu());
+            } catch (IdGameUnkownException e) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Id du jeu inconnu");
+            } catch (IdClientUnknownException e) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Id de client inconnu");
+            } catch (GameAlreadyOwnedException e) {
+                return ResponseEntity.status(HttpStatus.CONFLICT).body(achat.getIdClient() + " a déjà achété ce jeu");
+            }
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
     }
 
     @DeleteMapping(value = "/client/{id}")
-    public ResponseEntity<String> deleteClientAchats(@PathVariable String id) throws IdClientUnknownException{
+    public ResponseEntity<String> deleteClientAchats(@PathVariable String id, @RequestHeader(name="token") String token) throws IdClientUnknownException{
         return ResponseEntity.ok().body("ok");
     }
 
     @DeleteMapping(value = "/jeu/{id}")
-    public ResponseEntity<String> deleteJeuVentes(@PathVariable String id) throws IdClientUnknownException{
-        try{
-            serviceVente.deleteVentesJeu(Integer.valueOf(id));
-            return ResponseEntity.ok().body("Les ventes du jeu " + id + " ont été supprimées");
-        } catch (IdGameUnkownException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Id de jeu inconnu");
+    public ResponseEntity<String> deleteJeuVentes(@PathVariable String id, @RequestHeader(name="token") String token) throws IdClientUnknownException{
+        if(checkToken(token)){
+            try{
+                serviceVente.deleteVentesJeu(Integer.valueOf(id));
+                return ResponseEntity.ok().body("Les ventes du jeu " + id + " ont été supprimées");
+            } catch (IdGameUnkownException e) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Id de jeu inconnu");
+            }
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
     }
 
     public boolean checkToken(String token){
         HttpClient httpClient = HttpClient.newHttpClient();
-        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/client/token?token="+token)).GET().build();
         try {
+            HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/client/token?token="+token)).GET().build();
             HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
             if (response.statusCode() == 200){
                 return true;
@@ -118,6 +144,8 @@ public class ControllerVente {
         } catch (IOException e) {
             return false;
         } catch (InterruptedException e) {
+            return false;
+        } catch (Exception e) {
             return false;
         }
     }
